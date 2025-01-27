7a06befb919d7661c19fb4a2358a251668317100
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-01-27 18:36:58 +0530

    new code

--- 1.Non-primitive/1.Linear/1.bubble sort/3.insertion_sort.cpp
+++ 1.Non-primitive/1.Linear/1.bubble sort/3.insertion_sort.cpp
@@ -2,35 +2,49 @@
 using namespace std;
 class top
 {
-    public:void insertsort(int arr[],int s)
+    public:
+    void InsertionSort(int arr[] , int s)
     {
-        for(int i=1;i<s;i++)
+        for(int i = 1 ; i<s ; i++)
         {
-            int chotu=i;
-            while(chotu>=0 && arr[chotu]<arr[chotu-1])
+            int chotu = i;
+            while(chotu>0 && arr[chotu]<arr[chotu-1])
             {
-              int temp;
-              temp=arr[chotu-1];
-              arr[chotu-1]=arr[chotu];
-              arr[chotu]=temp;
-              chotu--;
+                int temp;
+                temp = arr[chotu-1];
+                arr[chotu-1] = arr[chotu];
+                arr[chotu] = temp;
+                chotu--;
             }
         }
+
     }
+
 };
+
 int main()
 {
     top p;
-    int a[]={5,4,2,1,3};
-    int s=sizeof(a)/sizeof(a[0]);
-    for(int i=0;i<s;i++)
+    int a[] = {4,5,6,7,3};
+    int s = sizeof(a) / sizeof(a[0]);
+    for(int i = 0 ; i < s ; i++)
     {
         cout<<a[i]<<"\t";
     }
-    p.insertsort(a,s);
-    cout<<"\n after applying \n";
-    for(int i=0;i<s;i++)
+    p.InsertionSort(a,s);
+    cout<<"\n After Appliying : \n";
+
+    for(int i=0 ; i<s ; i++)
     {
         cout<<a[i]<<"\t";
     }
+
 }
+
+
+
+
+
+
+
+// Spcce Complecity ---> yadi code me koi
